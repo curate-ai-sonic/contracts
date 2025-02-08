8b313b5a3a893c703a9139f0e6177005c0f2e18a
sushant-rumsan <sushant.tripathee@rumsan.net>
2025-02-08 18:38:54 +0545

    fixed-deployment-write

--- deploy/constants.ts
+++ deploy/constants.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config({path: '../.env'});
+
 export const CONTRACT = {
     PRIVATE_KEY: process.env.PRIVATE_KEY as string,
     PROVIDED_URL: process.env.PROVIDED_URL as string
--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,48 +1,39 @@
 import { ethers } from "ethers"
 import contractData from "./artifacts/ContentMediaToken.json"
 import postContractData from './artifacts/ContentMediaVoting.json'
-import settlementContractData from './artifacts/ContentMediaVoting.json'
+import settlementContractData from './artifacts/ContentMediaSettlement.json'
 import { CONTRACT } from "./constants"
-import * as fs from 'fs'
-import dotenv from 'dotenv'
-dotenv.config();
+import { writeToFile } from "./utils/writeContract"
 
 export const deployContract = async () => {
 
-    console.log(process.env.PRIVATE_KEY)
+    const provider = new ethers.JsonRpcProvider(CONTRACT.PROVIDED_URL)
+    const wallet = new ethers.Wallet(CONTRACT.PRIVATE_KEY, provider)
 
-    const provider = new ethers.JsonRpcProvider(CONTRACT.PROVIDED_URL);
-    const wallet = new ethers.Wallet(CONTRACT.PRIVATE_KEY, provider);
+    console.log("Deploying Contracts...")
 
     // Step 1: Deploy token contract
-    const tokenFactory = new ethers.ContractFactory(contractData.abi, contractData.bytecode, wallet);
-    const tokenContract = await tokenFactory.deploy(); 
-    const tokenContractAddress = await tokenContract.getAddress();
-    tokenContract.waitForDeployment()
+    const tokenFactory = new ethers.ContractFactory(contractData.abi, contractData.bytecode, wallet)
+    const tokenContract = await tokenFactory.deploy()
+    const tokenContractAddress = await tokenContract.getAddress()
+    await tokenContract.waitForDeployment()
 
     // Step 2: Deploy Post contract with token address
-    const postFactory = new ethers.ContractFactory(postContractData.abi, postContractData.bytecode, wallet);
-    const postContract = await postFactory.deploy(tokenContractAddress);
-    const postContractAddress = await postContract.getAddress();
+    const postFactory = new ethers.ContractFactory(postContractData.abi, postContractData.bytecode, wallet)
+    const postContract = await postFactory.deploy(tokenContractAddress)
+    const postContractAddress = await postContract.getAddress()
+    await postContract.waitForDeployment()
 
     // Step 3: Deploy settlement contract with token address and post contract address
-    const settleFactory = new ethers.ContractFactory(settlementContractData.abi, settlementContractData.bytecode, wallet);
-    const settleContract = await settleFactory.deploy(tokenContractAddress, postContractAddress);
-    const settleContractAddress = await settleContract.getAddress();
+    const settleFactory = new ethers.ContractFactory(settlementContractData.abi, settlementContractData.bytecode, wallet)
+    const settleContract = await settleFactory.deploy(tokenContractAddress, postContractAddress)
+    const settleContractAddress = await settleContract.getAddress()
+    await settleContract.waitForDeployment()
 
-    writeToFile('/', {tokenContractAddress, postContractAddress, settleContractAddress})
+    writeToFile('deployedContracts.json', {tokenContractAddress, postContractAddress, settleContractAddress})
 
-    console.log(`Token contract: ${tokenContractAddress}, Post contract: ${postContractAddress}, Settlement contract: ${settleContractAddress}`);
+    console.log(`Token contract: ${tokenContractAddress}, Post contract: ${postContractAddress}, Settlement contract: ${settleContractAddress}`)
 
-}
-
-
-const writeToFile = (filePath: string, newData: any) => {
-    const fileData = fs.readFileSync(filePath, 'utf8');
-    const data = JSON.parse(fileData);
-    data.push(newData);
-    fs.writeFileSync(filePath,
-        JSON.stringify(data, null, 2))
 }
 
 deployContract()
