6df2eb77008b7771040a12f68e90d9f4074c9f19
Sushant Tripathu <sushant@RS-L-25-02-Macair.local>
2025-02-25 17:02:50 +0545

    separate vote and post

--- contracts/roleManager.sol
+++ contracts/roleManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20; // Use latest stable version for security updates
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
--- test/curate-flow.ts
+++ test/curate-flow.ts
@@ -1,23 +1,28 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import {
-  CurateAIPostAndVote,
   CurateAIToken,
   CurateAISettlement,
   CurateAIRoleManager,
+  CurateAIPost,
+  CurateAIVote,
 } from "../typechain-types";
 import keccak256 from "keccak256";
 
 describe("ContentMediaVoting", function () {
-  let contentMediaVoting: CurateAIPostAndVote;
   let contentMediaToken: CurateAIToken;
   let contentMediaSettlement: CurateAISettlement;
   let curateAIRole: CurateAIRoleManager;
+  let curateAIPost: CurateAIPost;
+  let curateAIVote: CurateAIVote;
   let owner: any;
   let account1: any;
   let account2: any;
   let account3: any;
   let moderator1: any;
+
+  const CURATOR_ROLE = "CURATOR_ROLE";
+  const MODERATOR_ROLE = "MODERATOR_ROLE";
 
   const DAILY_MINT_AMOUNT = 100_000;
 
@@ -38,33 +43,40 @@ describe("ContentMediaVoting", function () {
       await curateAIRole.getAddress()
     )) as CurateAIToken;
 
-    const ContentMediaVotingFactory = await ethers.getContractFactory(
-      "CurateAIPostAndVote"
-    );
-    contentMediaVoting = (await ContentMediaVotingFactory.deploy(
-      await contentMediaToken.getAddress(),
+    const CurateAIPostFactory = await ethers.getContractFactory("CurateAIPost");
+    curateAIPost = (await CurateAIPostFactory.deploy(
       await curateAIRole.getAddress()
-    )) as CurateAIPostAndVote;
+    )) as CurateAIPost;
+
+    const CurateAIVoteFactory = await ethers.getContractFactory("CurateAIVote");
+    curateAIVote = (await CurateAIVoteFactory.deploy(
+      await contentMediaToken.getAddress(),
+      await curateAIRole.getAddress(),
+      await curateAIPost.getAddress()
+    )) as CurateAIVote;
 
     const ContentMediaSettlementFactory = await ethers.getContractFactory(
       "CurateAISettlement"
     );
     contentMediaSettlement = (await ContentMediaSettlementFactory.deploy(
       contentMediaToken.getAddress(),
-      contentMediaVoting.getAddress(),
+      curateAIVote.getAddress(),
       curateAIRole.getAddress()
     )) as CurateAISettlement;
 
     await curateAIRole.setSettlementContract(
       await contentMediaSettlement.getAddress()
     );
+    await curateAIRole.assignModerator(moderator1);
+    await curateAIRole
+      .connect(moderator1)
+      .assignCurator(await curateAIVote.getAddress());
   });
 
   describe("Add moderators", function () {
     it("Should add moderators in role manager contract", async function () {
-      await curateAIRole.assignModerator(moderator1);
       expect(
-        await curateAIRole.hasRole(keccak256("MODERATOR_ROLE"), moderator1)
+        await curateAIRole.hasRole(keccak256(MODERATOR_ROLE), moderator1)
       ).to.equal(true);
     });
   });
@@ -76,7 +88,7 @@ describe("ContentMediaVoting", function () {
       await curateAIRole.connect(moderator1).assignCurator(account3);
       await curateAIRole.connect(moderator1).assignCurator(owner);
       expect(
-        await curateAIRole.hasRole(keccak256("CURATOR_ROLE"), account1)
+        await curateAIRole.hasRole(keccak256(CURATOR_ROLE), account1)
       ).to.equal(true);
     });
   });
@@ -86,29 +98,29 @@ describe("ContentMediaVoting", function () {
       const contentHash = "QmTestHash";
       const tags = "blockchain,AI";
 
-      await expect(contentMediaVoting.createPost(contentHash, tags))
-        .to.emit(contentMediaVoting, "PostCreated")
+      await expect(curateAIPost.createPost(contentHash, tags))
+        .to.emit(curateAIPost, "PostCreated")
         .withArgs(1, owner.address, contentHash, tags);
     });
   });
 
   describe("Multiple Post Voting", function () {
     it("Should create 3 posts by 3 different accounts and vote on them", async function () {
-      await contentMediaVoting.connect(account1).createPost("QmPost1", "tag1");
-      await contentMediaVoting.connect(account2).createPost("QmPost2", "tag2");
-      await contentMediaVoting.connect(account3).createPost("QmPost3", "tag3");
+      await curateAIPost.connect(account1).createPost("QmPost1", "tag1");
+      await curateAIPost.connect(account2).createPost("QmPost2", "tag2");
+      await curateAIPost.connect(account3).createPost("QmPost3", "tag3");
 
       const vote1 = 500000;
       const vote2 = 49999;
       const vote3 = 9;
 
-      await contentMediaVoting.vote(2, vote1); // 1 post is already in above tests so the index is 2
-      await contentMediaVoting.vote(3, vote2);
-      await contentMediaVoting.vote(4, vote3);
+      await curateAIVote.vote(2, vote1); // 1 post is already in above tests so the index is 2
+      await curateAIVote.vote(3, vote2);
+      await curateAIVote.vote(4, vote3);
 
-      expect(await contentMediaVoting.getPostScore(2)).to.equal(vote1);
-      expect(await contentMediaVoting.getPostScore(3)).to.equal(vote2);
-      expect(await contentMediaVoting.getPostScore(4)).to.equal(vote3);
+      expect(await curateAIPost.getPostScore(2)).to.equal(vote1);
+      expect(await curateAIPost.getPostScore(3)).to.equal(vote2);
+      expect(await curateAIPost.getPostScore(4)).to.equal(vote3);
     });
   });
 
@@ -147,25 +159,44 @@ describe("ContentMediaVoting", function () {
     it("Should allow voting on multiple days and claim rewards correctly", async function () {
       const currentDay2 = await contentMediaSettlement.getCurrentDay();
 
-      await contentMediaVoting.vote(4, 1000);
+      const claimable_day_1_account_3 =
+        await contentMediaSettlement.getClaimableAmount(account3.address);
 
-      // This is day 3
+      await curateAIVote.vote(4, 1000);
+      await curateAIVote.vote(3, 1000);
+
+      // This is day 2
       await ethers.provider.send("evm_increaseTime", [86400]);
       await ethers.provider.send("evm_mine", []);
 
       await contentMediaSettlement.settleDay(currentDay2);
 
-      console.log(
-        "active days of account 3",
-        await contentMediaVoting.getUserVoteDays(account3)
-      );
+      const claimable_day_2_account_3 =
+        await contentMediaSettlement.getClaimableAmount(account3.address);
+
+      const claimable_day_2_account_2 =
+        await contentMediaSettlement.getClaimableAmount(account2.address);
+
+      await contentMediaSettlement.connect(account3).claimRewards();
+
+      // Day 3 starts
+      await ethers.provider.send("evm_increaseTime", [86400]);
+      await ethers.provider.send("evm_mine", []);
+      const claimable_day_3_account_3 =
+        await contentMediaSettlement.getClaimableAmount(account3.address);
 
       console.log(
-        "Claimable amount for account 3 on day 2: ",
-        await contentMediaSettlement.getClaimableAmount(account3.address)
+        "Claimable Amound: Day 1, account 3: ",
+        claimable_day_1_account_3
       );
-
-      await contentMediaSettlement.connect(account2).claimRewards();
+      console.log(
+        "Claimable Amound: Day 2, account 3: ",
+        claimable_day_2_account_3
+      );
+      console.log(
+        "Claimable Amound: Day 2, account 2: ",
+        claimable_day_2_account_2
+      );
     });
   });
 });
